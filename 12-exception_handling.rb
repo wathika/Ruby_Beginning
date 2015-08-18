c6ece2b8d33b7f8a1e78fd9a6a451af789603491
wathika <vcwathika@gmail.com>
2015-08-18 11:52:59 +0300

    exception with rescue

---
+++ 12-exception_handling.rb
@@ -0,0 +1,33 @@
+# The basic structure of exceptions looks like this
+
+# begin
+#   # perform some dangerous operation
+# rescue
+#   # do this if operation fails
+#   # for example, log the error
+# end
+
+names = ['bob', 'joe', 'steve', nil, 'frank']
+
+names.each do |name|
+  begin
+    puts "#{name}'s name has #{name.length} letters in it."
+  rescue
+    puts "Something went wrong!"
+  end
+end
+# when we go to call .length on nil we get an error. When an exception, or error,
+#   is raised, the rescue block will execute and then the program will continue to
+#    run as it normally would
+# if not our program would stop
+
+#use the rescue reserved word in-line like so:
+zero = 0
+puts "Before each call"
+zero.each { |element| puts element } rescue puts "Can't do that!"
+puts "After each call"
+# This code prints the following output:(cant call .each on an int)
+#
+# Before each call
+# Can't do that!
+# After each call

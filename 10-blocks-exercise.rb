9f02df9bb62252929a3b7a90e71754c632208bd2
wathika <vcwathika@gmail.com>
2015-08-18 12:49:50 +0300

    modified to work correctly

--- 10-blocks-exercise.rb
+++ 10-blocks-exercise.rb
@@ -8,3 +8,10 @@ execute { puts "Hello from inside the execute method!" }
 
 # Nothing is printed to the screen because the block is never activated with the
 # .call method. The method returns a Proc object.
+
+#modified to work correctly
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }

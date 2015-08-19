e5358b018fa7b1f6b8d7bbbe035dbbeb3e8b65ac
wathika <vcwathika@gmail.com>
2015-08-19 10:38:32 +0300

    used the select method to extract odd numbers to a new array

--- 13-arrays_hashes.rb
+++ 13-arrays_hashes.rb
@@ -1,6 +1,6 @@
 # Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 # and print out each value.
-def nums(arry)
-  arry.each { |x| puts x if x > 5}
-end
-nums([1,2,3,4,5,6,7,8,9,10])
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+  nums.each { |x| puts x if x > 5}
+  new_arry = nums.select{ |y| y % 2 != 0}

46cec41c952ea2abc1f0c9a0e1a3bbd949d31d11
wathika <vcwathika@gmail.com>
2015-08-22 23:52:25 +0300

    symbols in Ruby 1.9

--- 14-hashes_and_symbols.rb
+++ 14-hashes_and_symbols.rb
@@ -12,3 +12,33 @@ end
 no_nil_hash = Hash.new(""default val)
 
 my_first_symbol = :viscount        #creating a symbol
+
+#symbols are primarily used either as hash keys or for referencing method names.
+
+#Converting between strings and symbols is a snap.
+:sasquatch.to_s
+# ==> "sasquatch"
+
+"sasquatch".to_sym
+# ==> :sasquatch
+
+#converting an array of strings into symbols
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+# Add your code below!
+symbols = []
+strings.each do |s|
+    symbols.push(s.to_sym) # of .intern can be used
+end
+
+
+movies = {
+    :one => "1",
+    :two => "2",
+    :three => "3",
+    }
+#from old version of rocket to new in Ruby 1.9
+    movies = {
+        one: "1",
+        two: "2",
+        three: "3"
+        }

a2e703048b2cea7f9e1366701ba6737dd9ab4ed3
wathika <vcwathika@gmail.com>
2015-08-23 00:12:22 +0300

    iterarting over keys and values

--- 14-hashes_and_symbols.rb
+++ 14-hashes_and_symbols.rb
@@ -42,3 +42,25 @@ movies = {
         two: "2",
         three: "3"
         }
+
+#filterring a hash for values that meet a certain criteria
+grades = { alice: 100,
+        bob: 92,
+        chris: 95,
+        dave: 97
+      }
+
+      grades.select {|name, grade| grade < 97}
+      # ==> {:bob=>92, :chris=>95}
+
+      grades.select { |k, v| k == :alice }
+      # ==> {:alice=>100}
+
+#iterating over keys and values
+my_hash = { one: 1, two: 2, three: 3 }
+
+my_hash.each_key { |k| print k, " " }  #.each_key iterates over the key
+# ==> one two three
+
+my_hash.each_value { |v| print v, " " }  #.each_value iterates over the value
+# ==> 1 2 3
